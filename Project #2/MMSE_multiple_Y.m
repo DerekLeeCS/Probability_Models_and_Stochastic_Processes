19ee78800ce53c85f63bb3305663e6ff323bd862
Derek Lee <51767748+DerekLeeCS@users.noreply.github.com>
2021-03-14 00:38:33 -0500

    fixed a bug
    added a_o

--- Project #2/MMSE_multiple_Y.m
+++ Project #2/MMSE_multiple_Y.m
@@ -1,5 +1,7 @@
 function est = MMSE_multiple_Y( X, mu_x, mu_y, a )
 
+    a_o = mu_y * ( 1 - sum(a) );
     est = mu_y + ( sum(a.*X, 1) - mu_x );
+    est = est + a_o;
 
 end
--- Project #2/main.m
+++ Project #2/main.m
@@ -41,13 +41,23 @@ disp( "Linear: Successful" + newline() );
 
 theoretical_1 = MMSE_theoretical(X);
 
+% Prepare the table
+Bayesian = [ MSE_1_bayes_experimental; MSE_1_bayes_theoretical ];
+Linear = [ MSE_1_linear_experimental; MSE_1_linear_theoretical ];
+RowName = { 'Experimental'; 'Theoretical' };
+
+% Display the table
+T = table( Bayesian, Linear, 'RowNames', RowName );
+disp(T);
+
 
 %% Scenario 2
 
 % Constants
-numDraws = 1e4;
-numObservations = 5;
+numDraws = 1e5;
+numObservations = 10;
 mu_y = 1;
+mu_r = 0;
 mu_x = mu_y;
 
 % Variables
@@ -59,7 +69,6 @@ disp( "Starting Scenario 2..." );
 % Prepare the plot
 figure();
 hold on;
-ColOrd = get(gca,'ColorOrder');
 
 % Loop through pairs of variances
 for nObv = 1:numObservations
@@ -70,12 +79,9 @@ for nObv = 1:numObservations
     X = Y+R;
 
     % Covariances
-    cov_xx = cov(X.');
-    cov_xy = zeros( nObv, 1 );
-    for j = 1:nObv
-        temp = cov( X(j,:), Y );
-        cov_xy(j) = temp(2,2);
-    end
+    cov_xx = ones( nObv ) * ( var_y );
+    cov_xx = cov_xx + diag( ones( nObv,1 )*( var_r ) );
+    cov_xy = ones( nObv, 1 )*( var_y );
     
     % Calculate experimental MSE
     a = cov_xx \ cov_xy;    % Equiv to cov_xx^-1 * cov_xy
@@ -86,19 +92,19 @@ for nObv = 1:numObservations
     MSE_2_theoretical = var_y - ( (cov_xy).' * a );
     
     % Plot
-    line( [nObv,nObv], [ mean(MSE_2_experimental), MSE_2_theoretical ], ... 
-            'Color', ColOrd( mod(nObv,length(ColOrd)), : ), ...
+    line( [nObv,nObv], [ MSE_2_experimental, MSE_2_theoretical ], ... 
             'HandleVisibility', 'off');
-    plot( nObv, mean(MSE_2_experimental), 'or', 'HandleVisibility', 'off' );
+    plot( nObv, MSE_2_experimental, 'xr', 'HandleVisibility', 'off' );
     plot( nObv, MSE_2_theoretical, 'og', 'HandleVisibility', 'off' );
     
 end
 
 % Used for legend only
-plot( NaN,NaN,'or', 'DisplayName', 'Experimental' );
+plot( NaN,NaN,'xr', 'DisplayName', 'Experimental' );
 plot( NaN,NaN,'og', 'DisplayName', 'Theoretical' );
 
 % Label graph
+hold off;
 title( "MSE for Different Number of Observations (var_y = " + var_y + ...
         ", var_r = " + var_r + ")" );
 xlabel( "Number of Observations" );
